aaa863a52eb8c154ace8ee3ed2734bde15395cb1
sumit shinde <comp.sumit@gmail.com>
2018-09-29 11:19:52 +0530

    added exception classes and messages

---
+++ OnlineBankingSystem/src/com/cg/obs/util/Messages.java
@@ -0,0 +1,8 @@
+package com.cg.obs.util;
+
+public class Messages {
+
+	public static final String INCORRECT_INPUT_TYPE = "Please Enter Option as 'number' only!";
+	public static final String INCORRECT_CHOICE = "Incorrect choice entered! Try Again.";
+
+}

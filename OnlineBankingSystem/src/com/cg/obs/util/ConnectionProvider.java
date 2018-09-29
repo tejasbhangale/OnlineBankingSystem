ab0178b9df9233ab7b2b1fd6ff6e661c023f4813
V <ramya.b.v@capgemini.com>
2018-09-29 11:50:15 +0530

    modified connection provider

--- OnlineBankingSystem/src/com/cg/obs/util/ConnectionProvider.java
+++ OnlineBankingSystem/src/com/cg/obs/util/ConnectionProvider.java
@@ -45,10 +45,10 @@ public enum ConnectionProvider {
 			if (url != null && username != null && password != null) {
 				con = DriverManager.getConnection(url, username, password);
 			}else
-				throw new OnlineBankingException("Connection Configuaration Not Loaded!");
+				throw new OnlineBankingException(Messages.CONNECTION_CONFIGURATION_FAILURE);
 		} catch (SQLException e) {
 			log.error(e);
-			throw new OnlineBankingException("Connection is not established!");
+			throw new OnlineBankingException(Messages.CONNECTION_ESTABILISHED_FAILURE);
 		}
 		return con;
 	}
